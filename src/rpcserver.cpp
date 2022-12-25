0edc9e65a98633f42ec3f229855f7b51ba1e7603
ann0see <20726856+ann0see@users.noreply.github.com>
2022-12-25 21:47:43 +0100

    Success

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -35,7 +35,7 @@ CRpcServer::CRpcServer ( QObject* parent, QString strBindIP, int iPort, QString 
 {
     connect ( pTransportServer, &QTcpServer::newConnection, this, &CRpcServer::OnNewConnection );
 
-    /// @rpc_method jamulus/getVersionn2
+    /// @rpc_method jamulus/getVersion
     /// @brief Returns Jamulus version.
     /// @param {object} params - No parameters (empty object).
     /// @result {string} result.version - The Jamulus version.
