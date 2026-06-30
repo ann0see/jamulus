359c2795524da0df9bedae57a6b68c6dac92afaa
ann0see <20726856+ann0see@users.noreply.github.com>
2026-06-30 22:43:19 +0200

    Avoid container build

--- .github/autobuild/linux_deb.sh
+++ .github/autobuild/linux_deb.sh
@@ -81,7 +81,7 @@ setup() {
     echo "Installing dependencies..."
     sudo apt-get -qq update
     sudo apt-get -qq --no-install-recommends -y install devscripts build-essential debhelper fakeroot libjack-jackd2-dev qt6-base-dev qt6-multimedia-dev qt6-tools-dev
-    sudo apt-get install -y --reinstall qt6-base-dev qt6-tools-dev qt6-multimedia-dev
+
     setup_cross_compiler
 }
 
